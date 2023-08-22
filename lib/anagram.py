bbeaebdd7c4ec4163b881a29e1ee38dc5ea50f7f
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-22 15:14:05 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([char for char in word])
+        
+    def match(self, word_list):
+        pair = [wrd for wrd in word_list if sorted([char for char in wrd]) == self.word]
+        return pair
